3a2ea982def3ccd43ada0e387af5354f437b16ad
richardchen1115 <49451949+richardchen1115@users.noreply.github.com>
2021-06-03 01:14:02 +0800

    add units multi language supported

--- src/config.js
+++ src/config.js
@@ -19,4 +19,20 @@ export const Config = {
     getProp(langCode, 'tooltipText1'),
   tooltipText2: (langCode = defaultLangCode) =>
     getProp(langCode, 'tooltipText2'),
+  totalDistance: (langCode = defaultLangCode) =>
+    getProp(langCode, 'totalDistance'),
+  totalArea: (langCode = defaultLangCode) =>
+    getProp(langCode, 'totalArea'),
+  unit1: (langCode = defaultLangCode) =>
+    getProp(langCode, 'unit1'),
+  unit2: (langCode = defaultLangCode) =>
+    getProp(langCode, 'unit2'),
+  unit3: (langCode = defaultLangCode) =>
+    getProp(langCode, 'unit3'),
+  unit4: (langCode = defaultLangCode) =>
+    getProp(langCode, 'unit4'),
+  unit5: (langCode = defaultLangCode) =>
+    getProp(langCode, 'unit5'),
+  unit6: (langCode = defaultLangCode) =>
+    getProp(langCode, 'unit6'),
 };
